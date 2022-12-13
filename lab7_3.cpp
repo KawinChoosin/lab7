436c13dda98565284090925d879ff88df085ee60
Kawin <kawin_chusin@cmu.ac.th>
2022-12-13 16:59:00 +0700

    edit lab7_3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,27 @@
 #include<iostream>
 
 using namespace std;
-
+int adiff(int A,int B){
+  int c;
+  A=A%360;
+  B=B%360;
+  c=A-B;
+   if(c<0){
+    c=-c;
+  }
+  if(c>180){
+    c=(360-c);
+  }
+  return c;
+  }
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270)<<endl;
+  cout << adiff(210,45)<<endl;
+  cout << adiff(0,360)<<endl;
+  cout << adiff(10,350)<<endl;
+  cout << adiff(95,260)<<endl;
+  cout << adiff(90,-90)<<endl;
+  cout << adiff(1000,280)<<endl;
+  cout << adiff(60,244)<<endl;
 }
